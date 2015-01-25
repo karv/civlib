bd0ecf437e730fd32f4b08fcabfe3b81cf15c1c8
karv <karvayoEdgar@gmail.com>
2015-01-25 01:01:41 -0600

    Ciudad ahora puede agregar Propiedades con
    Ciudad.AgregaPropiedad(Propiedad)

---
+++ Ciudad/Propiedades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Civ
+{
+    public partial class Ciudad
+    {
+        /// <summary>
+        /// Agrega una instancia de <c>Propiedad</c> a la ciudad.
+        /// </summary>
+        /// <param name="Prop">Propiedad a agregar.</param>
+        public void AgregaPropiedad(Propiedad Prop)
+        {
+            if (!Propiedades.Contains(Prop)) { Propiedades.Add(Prop); }
+        }
+    }
+}
