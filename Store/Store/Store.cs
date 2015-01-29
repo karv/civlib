f675d8be7d70e4524a527d82e6575cedffe1a15f
karv <karvayoEdgar@gmail.com>
2015-01-28 21:06:14 -0600

    Más Formato

--- Store/Store/Store.cs
+++ Store/Store/Store.cs
@@ -12,33 +12,33 @@ namespace Store
 
 	public static partial class Store<T>
 	{
-        /// <summary>
-        /// Serializa un objeto a un archivo xml
-        /// </summary>
-        /// <param name="xml">Archivo en dónde guardar el xml</param>
-        /// <param name="Data">Datos a guardar.</param>
+		/// <summary>
+		/// Serializa un objeto a un archivo xml
+		/// </summary>
+		/// <param name="xml">Archivo en dónde guardar el xml</param>
+		/// <param name="Data">Datos a guardar.</param>
 		public static void Serialize(string xml, T Data)
 		{
 
-            var settings = new XmlWriterSettings { Indent = true, IndentChars = "\t", OmitXmlDeclaration= true };
-            XmlWriter wr = XmlWriter.Create(xml, settings);
-            DataContractSerializer serializer = new DataContractSerializer(typeof(T));
-            serializer.WriteObject(wr, Data);
-            wr.Close();
+			var settings = new XmlWriterSettings { Indent = true, IndentChars = "\t", OmitXmlDeclaration = true };
+			XmlWriter wr = XmlWriter.Create(xml, settings);
+			DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+			serializer.WriteObject(wr, Data);
+			wr.Close();
 		}
 
-        /// <summary>
-        /// Deserializa un objeto
-        /// </summary>
-        /// <param name="xml">nombre del archivo de dónde deserealizar.</param>
-        /// <returns>Devuelve un objeto con la información deserializada.</returns>
-		public static T Deserialize (string xml)
+		/// <summary>
+		/// Deserializa un objeto
+		/// </summary>
+		/// <param name="xml">nombre del archivo de dónde deserealizar.</param>
+		/// <returns>Devuelve un objeto con la información deserializada.</returns>
+		public static T Deserialize(string xml)
 		{
-            XmlTextReader xr = new XmlTextReader(new FileStream(xml, FileMode.Open));
-            DataContractSerializer deserializer = new DataContractSerializer(typeof(T));
-            object r = deserializer.ReadObject(xr);
-            xr.Close();            
-            return (T)r;           
+			XmlTextReader xr = new XmlTextReader(new FileStream(xml, FileMode.Open));
+			DataContractSerializer deserializer = new DataContractSerializer(typeof(T));
+			object r = deserializer.ReadObject(xr);
+			xr.Close();
+			return (T)r;
 		}
 	}
 }
