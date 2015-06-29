665bf69790fe87e4285f38e0fdeeb310e6c328b0
Edgar Carballo <karvayoEdgar@gmail.com>
2015-06-29 17:26:13 -0500

    Agrega funciones en la clase Ciudad que devuelve cuáles y cuántas unidades puede una ciudad reclutar.
    Ciudad.UnidadesConstruibles

--- Ciudad/Ciudad.cs
+++ Ciudad/Ciudad.cs
@@ -66,10 +66,45 @@ namespace Civ
 				AgregaPropiedad(x);
 			}
 		}
-		// Partial no asinado. 
+		// Partial no asinado.
 		/// <summary>
 		/// Terreno donde se contruye la ciudad.
 		/// </summary>
 		public Terreno Terr;
+
+		/// <summary>
+		/// Devuelve un nuevo diccionario cuyas entradas son el número de unidades que puede construir la ciudad, por cada unidad.
+		/// </summary>
+		/// <returns>The construibles.</returns>
+		public Dictionary<UnidadRAW, ulong> UnidadesConstruibles()
+		{
+			Dictionary <UnidadRAW, ulong> ret = new Dictionary<UnidadRAW, ulong>();
+
+			foreach (var x in Global.g_.Data.Unidades)
+			{
+				if (x.ReqCiencia == null || CivDueno.Avances.Contains(x.ReqCiencia))
+				{
+					ret.Add(x, UnidadesConstruibles(x));
+				}
+			}
+			return ret;
+		}
+
+		/// <summary>
+		/// Devuelve la cantidad de unidades que puede construir esta ciudad de una unidadRAW específica.
+		/// Tiene en cuenta sólo los recursos.
+		/// </summary>
+		/// <returns>The construibles.</returns>
+		/// <param name="unid">Unid.</param>
+		public ulong UnidadesConstruibles(UnidadRAW unid)
+		{
+			ulong max = ulong.MaxValue;
+			foreach (var y in unid.Reqs)
+			{
+				// ¿Cuántas unidades puede hacer, limitando por recursos?
+				max = (ulong)Math.Min(max, Almacen[y.Key] / y.Value);
+			}
+			return max;
+		}
 	}
 }
