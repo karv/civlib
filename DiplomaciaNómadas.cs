358725144591db0e7c7141ed80eabaddeba91b8f
Edgar Carballo <karvayoEdgar@gmail.com>
2015-09-19 17:04:27 -0500

    Implementar una diplomacia para nómadas: Siempre pueden atacar

---
+++ DiplomaciaNómadas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Civ
+{
+	public class DiplomaciaNómadas:IDiplomacia
+	{
+		public DiplomaciaNómadas()
+		{
+		}
+
+		public bool PermiteAtacar(Armada arm)
+		{
+			return true;
+		}
+	}
+}
+
