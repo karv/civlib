49156dd47e7807141bf2566b1f59a55d03d26fbb
Edgar Carballo <karvayoEdgar@gmail.com>
2015-09-18 21:31:14 -0500

    Agregar IDiplomacia

---
+++ IDiplomacia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Civ;
+
+namespace Civ
+{
+	/// <summary>
+	/// Interfaz para decidir cómo comportarse diplomáticamente
+	/// </summary>
+	public interface IDiplomacia
+	{
+		bool PermiteAtacar(Armada arm);
+	}
+}
+
