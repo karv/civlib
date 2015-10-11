9e7856851dbe2fa6625d1bb7de7348ec032569ce
Edgar Carballo <karvayoEdgar@gmail.com>
2015-10-11 17:16:59 -0500

    Agregar interface común de objetos Data

---
+++ IImportable.cs
@@ -0,0 +1,20 @@
+﻿namespace Civ.Data.Import
+{
+	/// <summary>
+	/// Representa un objeto que puede ser importado desde un archivo de texto
+	/// </summary>
+	public interface IImportable
+	{
+		/// <summary>
+		/// Construye este objeto a partir d un archivo de textoe 
+		/// </summary>
+		/// <param name="file">Archivo de data</param>
+		void Importar (string file);
+
+		/// <summary>
+		/// Devuelve el identificador del objeto, para hacer referecias cruzadas
+		/// </summary>
+		string Id { get; }
+	}
+}
+
