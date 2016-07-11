979029eff9fd23b0c49cf0f1f74d2bee13274967
Edgar Carballo <karvayoEdgar@gmail.com>
2016-07-11 18:41:43 -0500

    No enviar null en un EventArgs

--- Global/Estáticos.cs
+++ Global/Estáticos.cs
@@ -66,7 +66,7 @@ namespace Civ.Global
 				_pausado = value;
 				if (!value)
 					timer = DateTime.Now; // Al despausar se reestablece el timer
-				AlCambiarEstadoPausa?.Invoke (this, null);
+				AlCambiarEstadoPausa?.Invoke (this, EventArgs.Empty);
 			}
 		}
 
